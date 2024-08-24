e0d5d5740a208de9c14bbbb9c47ad9c05b3b7ee0
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-08-25 01:17:43 +0600

    add images

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
   return (
     <div className="container mx-auto">
       <div  data-aos="fade-up" data-aos-duration="1000" className=" carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full h-[50%]"> 
+        <div id="slide1" className="carousel-item relative w-full h-[50%] rounded"> 
           <img src={cover3} className="w-full  object-fill resize" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
@@ -18,7 +18,7 @@ const Banner = () => {
             </a>
           </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full h-[50%]">
+        <div id="slide2" className="carousel-item relative w-full h-[50%] rounded">
           {/* <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" /> */}
           <img src={cover2} className="w-full object-fill resize" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -30,7 +30,7 @@ const Banner = () => {
             </a>
           </div>
         </div>
-        <div id="slide3" className="carousel-item relative w-full h-[50%]">
+        <div id="slide3" className="carousel-item relative w-full h-[50%] rounded">
           {/* <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" /> */}
           <img src={cover1} className="w-full object-fill resize" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
--- src/component/SingleProperty/SingleProperty.jsx
+++ src/component/SingleProperty/SingleProperty.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaDollarSign } from "react-icons/fa";
+// import { FaDollarSign } from "react-icons/fa";
 import PropTypes from "prop-types";
 
 const SingleProperty = ({ property }) => {
-  const { id, estate_title, segment_name, image, status, area, facilities } = property;
+  const { id, name, image, price, category } = property;
   return (
     <article className="flex flex-col dark:bg-gray-50">
       
@@ -18,27 +18,27 @@ const SingleProperty = ({ property }) => {
         <Link to={id}></Link>
         
         <h3 data-aos="fade-right" className="flex-1 py-2 text-lg font-semibold leading-snug">
-          {estate_title}
+          {name}
         </h3>
-        <p className="text-green-300 font-semibold text-xl"> {segment_name}</p>
-        <div className=" flex justify-between gap-2">
+        <p className=" font-bold text-xl text-orange-800"> {price}/=</p>
+        {/* <div className=" flex justify-between gap-2">
           {facilities.map((tag, idx) => (
-            <span className=" text-teal-400" key={idx}>
+            <span className="  font-semibold" key={idx}>
               {tag}
             </span>
           ))}
-        </div>
+        </div> */}
         
         <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-600">
-          <span className="font-bold text-teal-500 text-xl">{status}</span>
+          <span className="font-bold text-orange-600 text-xl">{category}</span>
           <div className="flex gap-1 items-center">
             <span>
-            <FaDollarSign />
+            {/* <FaDollarSign /> */}
 
             </span>
-            <span className="font-bold text-teal-500 text-xl">{area}</span>
+            {/* <span className="font-bold text-orange-500 text-xl">{area}</span> */}
           </div>
-          <Link to={`/property/${id}`}><button  data-aos="flip-up" className="btn text-white bg-teal-400 ">View products</button></Link>
+          <Link to={`/property/${id}`}><button  data-aos="flip-up" className="btn text-white bg-yellow-600 ">View products</button></Link>
         </div>
       </div>
     </article>
